9cb018c7fdf22beb9d8b106ef92096ab1bb89943
Quinn N <88954051+quinn-n@users.noreply.github.com>
2024-05-16 16:16:46 -0400

    Migrate to new eslint config
    Migrate to the new eslint config style and switch to strict linting

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+
+// NextJS's ESLint integration has some design flaws:
+// https://github.com/vercel/next.js/discussions/59347#discussioncomment-9448488
+// Unfortunately using a custom ESLint config means losing `next lint`
+const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  }
+};
 
 export default nextConfig;
