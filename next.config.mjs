ca50320f1311a2bc0a9b5fa90a1d006107270510
Quinn N <88954051+quinn-n@users.noreply.github.com>
2026-04-11 22:42:30 -0400

    `eslint` is no longer a supported part of `nextConfig`
    `next lint` was removed entirely and now just uses eslint.

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-// NextJS's ESLint integration has some design flaws:
-// https://github.com/vercel/next.js/discussions/59347#discussioncomment-9448488
-// Unfortunately using a custom ESLint config means losing `next lint`
-const nextConfig = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
